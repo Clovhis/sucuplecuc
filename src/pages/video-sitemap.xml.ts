2a6c467367dd8b37f3f90ae675e27e56961735cc
Leonardo R Vargas <yosoyvargas@hotmail.com>
2026-06-20 08:04:13 -0300

    Fix Search Console video and indexing signals (#6)

--- src/pages/video-sitemap.xml.ts
+++ src/pages/video-sitemap.xml.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro';
 import { getMovieTrailerPath, getMovies, getYoutubeEmbedUrl, getYoutubeThumbnailUrl } from '../lib/movies';
-import { SITE_URL } from '../lib/seo';
+import { getBestKnownMoviePublicationDate, SITE_URL } from '../lib/seo';
 
 export const prerender = true;
 
@@ -24,6 +24,7 @@ export const GET: APIRoute = () => {
 			const watchPageUrl = toAbsoluteUrl(getMovieTrailerPath(movie.slug));
 			const playerUrl = getYoutubeEmbedUrl(movie.trailerYoutubeId);
 			const thumbnailUrl = getYoutubeThumbnailUrl(movie.trailerYoutubeId);
+			const publicationDate = getBestKnownMoviePublicationDate(movie);
 			const videoTitle = `Trailer oficial de ${movie.title} (${movie.year})`;
 			const videoDescription = `Mirá el trailer oficial de ${movie.title} (${movie.year}) y volvés a la reseña completa en Cine Posta.`;
 
@@ -31,7 +32,7 @@ export const GET: APIRoute = () => {
 				return '';
 			}
 
-			return `  <url>\n    <loc>${escapeXml(watchPageUrl)}</loc>\n    <video:video>\n      <video:thumbnail_loc>${escapeXml(thumbnailUrl)}</video:thumbnail_loc>\n      <video:title>${escapeXml(videoTitle)}</video:title>\n      <video:description>${escapeXml(videoDescription)}</video:description>\n      <video:player_loc>${escapeXml(playerUrl)}</video:player_loc>\n      <video:requires_subscription>no</video:requires_subscription>\n    </video:video>\n  </url>`;
+			return `  <url>\n    <loc>${escapeXml(watchPageUrl)}</loc>\n    <video:video>\n      <video:thumbnail_loc>${escapeXml(thumbnailUrl)}</video:thumbnail_loc>\n      <video:title>${escapeXml(videoTitle)}</video:title>\n      <video:description>${escapeXml(videoDescription)}</video:description>\n      <video:player_loc>${escapeXml(playerUrl)}</video:player_loc>\n      <video:publication_date>${escapeXml(publicationDate)}</video:publication_date>\n      <video:requires_subscription>no</video:requires_subscription>\n    </video:video>\n  </url>`;
 		})
 		.filter(Boolean)
 		.join('\n');
